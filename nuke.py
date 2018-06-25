6c749e31e612edcac4dc146534f3419cdb591593
Ryan Whelan <4249368+rwhelan@users.noreply.github.com>
2018-06-25 15:50:00 -0400

    rename global flag to something more obvious

--- nuke.py
+++ nuke.py
@@ -10,8 +10,7 @@ if len(sys.argv) <= 1:
     print("No operation given", file = sys.stderr)
     sys.exit(1)
 
-
-global_services = True if (len(sys.argv) == 3) and (sys.argv[2] == 'global') else False
+enable_global_services = True if (len(sys.argv) == 3) and (sys.argv[2] == 'global') else False
 
 
 ### Import all the nukers
@@ -30,7 +29,7 @@ for module_file in os.listdir(module_path):
     __import__("nukes."+module_name)
 
 
-if global_services:
+if enable_global_services:
     enabled_modules = [i() for i in BaseNuker.__subclasses__() if i.enabled]
 else:
     enabled_modules = [i() for i in BaseNuker.__subclasses__() if i.enabled and i.global_service == False]
@@ -63,5 +62,3 @@ op = ops.get(sys.argv[1], ops['_unknown'])
 
 ### Go!
 op(sorted_modules)
-
-print(global_services)
